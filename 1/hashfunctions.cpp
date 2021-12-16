b98792ecaa44cd31a9ee32663ca017aaa7682747
Uskova Taisia <uskowa14@gmail.com>
2021-12-16 20:17:20 +0300

    All

---
+++ 1/hashfunctions.cpp
@@ -0,0 +1,31 @@
+#include "hashfunctions.h"
+#include <iostream>
+
+
+uint32_t adler32( std::istream &file ){
+    uint32_t s1 = 1;
+    uint32_t s2 = 0;
+    unsigned char ch;
+    while( !file.eof() ){
+        file.read((char *)(&ch), sizeof(unsigned char));
+        s1 = ( s1 + (ch) ) % 65521;
+        s2 = ( s2 + s1 ) % 65521;
+    }
+    return ( s2 << 16 ) | s1;
+}
+
+uint64_t sum64( std::istream &file ){
+    uint64_t sum = 0, end, result = 0;
+    unsigned char ch;
+    while (!file.eof()){
+        end = 0;
+        result = 0;
+        while (!file.eof() && end < 8) {
+            file.read((char *) (&ch), sizeof(uint64_t));
+            result = (result << 8) | ch;
+            end++;
+        }
+        sum += result;
+    }
+    return sum;
+}

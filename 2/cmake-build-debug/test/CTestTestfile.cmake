2d57731a800c406781b240d2d6d3c3da24e75524
Tatame <71048870+Tatame@users.noreply.github.com>
2022-01-08 16:09:36 +0300

    Delete 2/cmake-build-debug directory

--- 2/cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Documents/engnr-nsu-cpp-2021/2/test
-# Build directory: C:/Users/User/Documents/engnr-nsu-cpp-2021/2/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[googletest]=] "C:/Users/User/Documents/engnr-nsu-cpp-2021/2/cmake-build-debug/test/calculator_tests.exe")
-set_tests_properties([=[googletest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/Documents/engnr-nsu-cpp-2021/2/test/CMakeLists.txt;10;add_test;C:/Users/User/Documents/engnr-nsu-cpp-2021/2/test/CMakeLists.txt;0;")

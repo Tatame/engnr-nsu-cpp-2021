acb79d605a423b066cea12f1dcff501d8d06c206
Tatame <71048870+Tatame@users.noreply.github.com>
2022-02-20 19:37:08 +0300

    Delete 3/cmake-build-debug directory

--- 3/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/last_war/3
-# Build directory: C:/Users/User/last_war/3/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[googletests]=] "C:/Users/User/last_war/3/cmake-build-debug/List_tests.exe")
-set_tests_properties([=[googletests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/last_war/3/CMakeLists.txt;20;add_test;C:/Users/User/last_war/3/CMakeLists.txt;0;")
-subdirs("googletest")

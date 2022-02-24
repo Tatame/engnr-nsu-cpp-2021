fa8662df21a6a58b481a25f5d643138d9a6b8366
Uskova Taisia <uskowa14@gmail.com>
2022-02-24 11:16:41 +0300

    Revert "Update build.bat"
    This reverts commit f6e6ea560b8c21cf72f82c9fb749e7b13fa8f17b.

--- .github/buildsteps/windows/build.bat
+++ .github/buildsteps/windows/build.bat
@@ -5,6 +5,21 @@ cd build                                                      || exit /b 1
 cmake --version                                               || exit /b 1
 
 @echo.
+@echo ============================= BUILD x86_32 ==============================
+cmake -E rm -rf "x86_32"                                      || exit /b 1
+cmake -G "Visual Studio 16 2019" -A Win32 -B "x86_32" -S ".." || exit /b 1
+@echo.
+@echo.                             x86_32 (Debug)
+cmake --build "x86_32" --config Debug                         || exit /b 1
+@echo.
+@echo.                            x86_32 (Release)
+cmake --build "x86_32" --config Release                       || exit /b 1
+
+@echo.
+@echo x86_32 configurations (Debug and Release) have been built successfully.
+@echo =========================================================================
+
+@echo.
 @echo.
 
 @echo ============================= BUILD x86_64 ==============================
